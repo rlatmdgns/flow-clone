1e7cd80dc85f8f2293d556680362e6514dbb7a75
kimseunghun <rlatmdgns94@naver.com>
2021-03-26 02:54:38 +0900

    firebase 연동 및 회원가입

--- sagas/user.js
+++ sagas/user.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import { authService } from "../firebase";
 import { all, fork, put, call, takeLatest } from "redux-saga/effects";
 import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE } from '../reducers/user';
 
-function signUpAPI(data) {
-  return axios.post('/signup', data);
-}
 
 function* signUp(action) {
   try {
-    const result = yield call(signUpAPI, action.data);
-    console.log("result",result);
+    yield call(
+      [authService, authService.createUserWithEmailAndPassword],
+      action.data.email,
+      action.data.password
+    );
     yield put({
       type: SIGN_UP_SUCCESS,
     });
