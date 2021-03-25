98a2edbc2ae35f3df768bdd7138c5e8e1414e30f
kimseunghun <rlatmdgns94@naver.com>
2021-03-26 03:20:50 +0900

    회원가입

--- sagas/user.js
+++ sagas/user.js
@@ -3,13 +3,18 @@ import { all, fork, put, call, takeLatest } from "redux-saga/effects";
 import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE } from '../reducers/user';
 
 
+function signUpAPI(data) {
+  authService.createUserWithEmailAndPassword(data.email, data.password)
+}
+
 function* signUp(action) {
   try {
-    yield call(
-      [authService, authService.createUserWithEmailAndPassword],
-      action.data.email,
-      action.data.password
-    );
+    yield call(signUpAPI, action.data); 
+    // yield call(
+    //   [authService, authService.createUserWithEmailAndPassword],
+    //   action.data.email,
+    //   action.data.password
+    // );
     yield put({
       type: SIGN_UP_SUCCESS,
     });
