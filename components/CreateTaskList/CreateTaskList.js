9d6a23a11ce762a3be4534f9be39b0a3a17e0a7c
kimseunghun <rlatmdgns94@naver.com>
2021-04-23 02:57:44 +0900

    date format timestamp

--- components/CreateTaskList/CreateTaskList.js
+++ components/CreateTaskList/CreateTaskList.js
@@ -4,16 +4,22 @@ import Flatpickr from 'react-flatpickr';
 import TaskStateGroup from '../TaskStateGroup';
 import { TaskList, TaskItemTitle, TaskListCell } from './styles';
 import 'flatpickr/dist/themes/material_green.css';
+import moment from 'moment';
 
 const CreateTaskList = () => {
   const [startDate, setStartDate] = useState('');
+  console.log('startDate', moment(startDate[0]).format('YYYY-MM-DDTHH:mm:ss'));
   const [endDate, setEndDate] = useState('');
-  console.log(startDate.toLocaleString());
+  console.log('endDate', moment(endDate[0]).format('YYYY-MM-DDTHH:mm:ss'));
+
   const stateHandler = () => {
     console.log('andidy');
   };
-  const dateClear = () => {
+  const startDateClear = () => {
     setStartDate('');
+  };
+  const endDateClear = () => {
+    setEndDate('');
   };
   return (
     <>
@@ -32,17 +38,33 @@ const CreateTaskList = () => {
       <TaskList>
         <TaskItemTitle>시작일시</TaskItemTitle>
         <TaskListCell>
-          <Flatpickr data-enable-time value={startDate} options={{ minDate: new Date() }} onChange={(date) => setStartDate(date)} />
-          {startDate.toLocaleString()}
-          <button onClick={dateClear}>지우기</button>
+          <Flatpickr
+            data-enable-time
+            value={startDate}
+            options={
+              {
+                minDate: new Date(),
+                dateFormat: 'Y-m-d H:i',
+              }
+            }
+            onChange={(date) => setStartDate(date)}
+          />
+          {startDate && <button onClick={startDateClear}>지우기</button>}
         </TaskListCell>
         <TaskItemTitle>마감일시</TaskItemTitle>
         <TaskListCell>
           <Flatpickr
             data-enable-time
             value={endDate}
+            options={
+              {
+                minDate: new Date(),
+                dateFormat: 'Y-m-d H:i',
+              }
+            }
             onChange={(date) => setEndDate(date)}
           />
+          {endDate && <button onClick={endDateClear}>지우기</button>}
         </TaskListCell>
       </TaskList>
     </>
